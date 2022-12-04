ac4eef715d4a0b78953546d4cd6fbf78ffb2fd40
Madhu @ EIT <madhu@essentialit.co.nz>
2022-12-05 10:56:27 +1300

    New Scripts

---
+++ Exchange/ManageCalendarPermissionsViaGroups/Remove-AccountPermissions.ps1
@@ -0,0 +1,18 @@
+param
+(
+    [Parameter(Mandatory=$True)]
+    [String] $DistributionGroupName,
+    [Parameter(Mandatory=$True)]
+    [String] $OldAccount
+)
+
+$AllMembers = Get-DistributionGroupMember -Identity $DistributionGroupName `
+    | Select-Object DisplayName, PrimarySmtpAddress `
+    | Sort-Object DisplayName
+
+foreach ($Member in $AllMembers)
+{
+    Write-Host "$($Member.DisplayName)" -ForegroundColor Green
+    Remove-MailboxFolderPermission -Identity $($Member.PrimarySmtpAddress + ":\Calendar") -User $OldAccount -AccessRights $Permissions -Confirm:$false
+    Write-Host "`n"
+}
