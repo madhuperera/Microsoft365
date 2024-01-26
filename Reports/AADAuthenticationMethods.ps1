1a3a2b5d80d94a3f64a3efd4f361549cf6d295ca
Madhu Perera <37104267+madhuperera@users.noreply.github.com>
2024-01-26 15:14:26 +1300

    Update AADAuthenticationMethods.ps1

--- Reports/AADAuthenticationMethods.ps1
+++ Reports/AADAuthenticationMethods.ps1
@@ -1,1 +1,19 @@
+# Connect to Microsoft Graph with the UserAuthenticationMethod.Read.All permission scope
+Connect-Graph -Scopes "UserAuthenticationMethod.Read.All"
 
+# Get all users in Azure AD
+$AllUsers = Get-MgUser
+
+
+# Loop through each user and retrieve their authentication methods
+foreach ($Member in $AllUsers)
+{
+    $MemberId = $Member.Id
+    $MemberName = $Member.DisplayName
+    Write-Host "Windows Hello for Business for $MemberName:"
+    $AuthMethods = Get-MgUserAuthenticationMethod -UserId $MemberId
+    foreach ($Method in $AuthMethods)
+    {
+        
+    }
+}
