687b9fcf023858e8188739b33a3264d831925e55
Madhu Perera <37104267+madhuperera@users.noreply.github.com>
2024-01-26 15:40:05 +1300

    Update AADAuthenticationMethods.ps1

--- Reports/AADAuthenticationMethods.ps1
+++ Reports/AADAuthenticationMethods.ps1
@@ -4,6 +4,7 @@ Connect-Graph -Scopes "UserAuthenticationMethod.Read.All"
 # Get all users in Azure AD
  $AllUsers = Get-MgUser -Filter "accountEnabled eq true"
 
+$AllData = @()
 
 # Loop through each user and retrieve their authentication methods
 foreach ($Member in $AllUsers)
@@ -11,9 +12,17 @@ foreach ($Member in $AllUsers)
     $MemberId = $Member.Id
     $MemberName = $Member.DisplayName
     Write-Host "Windows Hello for Business for $($MemberName):"
-    $WHfBAuthMethods = Get-MgUserAuthenticationMethod -UserId $MemberId | Where-Object {$_.additionalProperties.'@odata.type' -like "*WindowsHelloForBusiness*"}
+    $WHfBAuthMethods = Get-MgUserAuthenticationMethod -UserId $MemberId | Where-Object {$_.additionalProperties.'@odata.type' -like "*windowsHelloForBusinessAuthenticationMethod*"}
     foreach ($Method in $WHfBAuthMethods)
     {
-        $Method
+        $DeviceName = $Method.additionalProperties.displayName
+        $RegisteredDate = $Method.additionalProperties.createdDateTime
+
+        $OBJ = New-Object PSObject
+        $OBJ | Add-Member -MemberType NoteProperty -Name "STaffDisplayName" -Value $MemberName
+        $OBJ | Add-Member -MemberType NoteProperty -Name "DeviceName" -Value $DeviceName
+        $OBJ | Add-Member -MemberType NoteProperty -Name "RegisteredDate" -Value $RegisteredDate
+
+        $AllData += $OBJ
     }
 }
