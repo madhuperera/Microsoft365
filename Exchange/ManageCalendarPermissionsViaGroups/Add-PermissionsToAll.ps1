6ce4757cdaed8e0bf0f653e344211bced8719bfc
Madhu Perera <madhu@sonitlo.com>
2025-01-17 22:26:28 +1300

    New Script

---
+++ Exchange/ManageCalendarPermissionsViaGroups/Add-PermissionsToAll.ps1
@@ -0,0 +1,18 @@
+param
+(
+    [Parameter(Mandatory=$True)]
+    [String] $StaffMemberPrimarySmtpAddress,
+    [Parameter(Mandatory=$True)]
+    [String] $Permissions
+)
+
+$AllMembers = Get-Mailbox | Where-Object {$_.RecipientTypeDetails -eq "UserMailbox"}`
+    | Select-Object DisplayName, PrimarySmtpAddress `
+    | Sort-Object DisplayName
+
+foreach ($Member in $AllMembers)
+{
+    Write-Host "$($Member.DisplayName)" -ForegroundColor Green
+    Add-MailboxFolderPermission -Identity $($Member.PrimarySmtpAddress + ":\Calendar") -User $StaffMemberPrimarySmtpAddress -AccessRights $Permissions
+    Write-Host "`n"
+}
